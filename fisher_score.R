3ea351dbcb39b0cbb9896d550da77230dd11634b
ganyue_laptop <gany.fml@gmail.com>
2015-03-23 10:15:39 -0500

    little correction

--- fisher_score.R
+++ fisher_score.R
@@ -2,13 +2,12 @@
 cmap_data_mat = replicate(3, rnorm(1000))
 rownames(cmap_data_mat) <- rownames(cmap_data_mat, do.NULL = FALSE, prefix = "tag")
 colnames(cmap_data_mat) <- c("Drug","Disease","Control")
-cmap
 
 #convert to CMAPCollection
 cmap <- CMAPCollection(cmap_data_mat)
-
 #induce
 cmap <- induceCMAPCollection(cmap, "members", lower=-1, higher=1)
+cmap
 
 #calculate fisher score and display the zscore
 result.list <- fisher_score(cmap, cmap, universe = featureNames( cmap))
